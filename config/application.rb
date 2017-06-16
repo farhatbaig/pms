b864f3cf58f57546926b2334dce84b1bc1511df4
Farhat Baig <farhat.baig@46labs.com>
2017-06-17 01:06:47 +0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -67,6 +67,7 @@ module RedmineApp
     # See http://guides.rubyonrails.org/caching_with_rails.html#cache-stores
     # for more options (same options as config.cache_store).
     config.redmine_search_cache_store = :memory_store
+    config.assets.initialize_on_precompile = false
 
     # Configure log level here so that additional environment file
     # can change it (environments/ENV.rb would take precedence over it)
